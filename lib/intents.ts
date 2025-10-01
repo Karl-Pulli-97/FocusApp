40225c45cd03a93756d087b1e38bf38c0d78bce5
Karl Pulli <kalle.pulli@hotmail.com>
2025-10-01 18:38:56 +0200

    Added DnD for android

--- lib/intents.ts
+++ lib/intents.ts
@@ -1,1 +1,17 @@
 // Hantering av DND inställningar.
+
+import { Platform, Linking } from 'react-native';
+import * as IntentLauncher from 'expo-intent-launcher';
+
+
+export function openSystemFocusSettings() {
+    if (Platform.OS === 'android') {
+        try {
+            IntentLauncher.startActivityAsync(IntentLauncher.ActivityAction.NOTIFICATION_POLICY_ACCESS_SETTINGS);
+        } catch {
+            IntentLauncher.startActivityAsync(IntentLauncher.ActivityAction.APP_NOTIFICATION_SETTINGS);
+        }
+    } else {
+        Linking.openSettings();
+    }
+}
